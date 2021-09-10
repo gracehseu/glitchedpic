f3d0777b812c8b1f13d22423996acc4aad9ce08b
Grace Hseu <25470155+gracehseu@users.noreply.github.com>
2021-09-09 23:41:20 -0500

    updated random pixel swap to squares of size 10

--- glitches/randomPixelSwap.py
+++ glitches/randomPixelSwap.py
@@ -10,6 +10,7 @@ class randomPixelSwap(ImageGlitcherInterface):
 
     def glitch_image(self, image_name):
         print(str(image_name))
+        image_name = "Distant View of Niagara Falls.jpeg"
         im = Image.open("images/" + image_name)
 
         image_arr = np.asarray(im).copy()
@@ -18,13 +19,16 @@ class randomPixelSwap(ImageGlitcherInterface):
         print(im_width, im_height)
 
         # only swap like 1/8 of the image
-        num_swap = int(im_width * im_height / 8)
+        num_swap = int(im_width * im_height / 2000)
         for i in range(num_swap):
-            swap_height_1 = random.randint(0, im_height - 1)
-            swap_width_1 = random.randint(0, im_width - 1)
-            swap_height_2 = random.randint(0, im_height - 1)
-            swap_width_2 = random.randint(0, im_width - 1)
-            image_arr[swap_height_2][swap_width_2], image_arr[swap_height_1][swap_width_1] = image_arr[swap_height_1][swap_width_1], image_arr[swap_height_2][swap_width_2]
+            swap_height_1 = random.randint(0 + 10, im_height - 1 - 10)
+            swap_width_1 = random.randint(0 + 10, im_width - 1 - 10)
+            swap_height_2 = random.randint(0 + 10, im_height - 1 - 10)
+            swap_width_2 = random.randint(0 + 10, im_width - 1 - 10)
+            # image_arr[swap_height_2][swap_width_2], image_arr[swap_height_1][swap_width_1] = image_arr[swap_height_1][swap_width_1], image_arr[swap_height_2][swap_width_2]
+            for i in range(swap_height_1 - 10, swap_height_1):
+                for j in range( swap_width_1 - 10, swap_width_1):
+                    image_arr[i][j], image_arr[swap_height_2 - i][swap_width_2 - j] = image_arr[swap_height_2 - i][swap_width_2 - j], image_arr[i][j]
 
         new_image = Image.fromarray(image_arr, 'RGB')
         self.save_image(self.image_glitch_type + image_name, new_image)
