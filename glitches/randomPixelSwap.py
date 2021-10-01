3b3ff8b0fb1e8c2775b234a22edc5b10dcff9d23
Grace Hseu <25470155+gracehseu@users.noreply.github.com>
2021-09-30 21:26:38 -0500

    modified randomPixelSwap

--- glitches/randomPixelSwap.py
+++ glitches/randomPixelSwap.py
@@ -16,14 +16,18 @@ class randomPixelSwap(ImageGlitcherInterface):
 
         im_width, im_height = im.size
         print(im_width, im_height)
-
+        pixel_size = im_height // 25 if im_height > im_width else  im_width // 25
         # only swap like 1/8 of the image
-        num_swap = int(im_width * im_height / 2000)
+        num_swap = int(im_width * im_height / 3000)
         for i in range(num_swap):
-            swap_height_1 = random.randint(0 + 10, im_height - 1 - 10)
-            swap_width_1 = random.randint(0 + 10, im_width - 1 - 10)
-            swap_height_2 = random.randint(0 + 10, im_height - 1 - 10)
-            swap_width_2 = random.randint(0 + 10, im_width - 1 - 10)
+            swap_height_1 = random.randint(
+                0 + pixel_size, im_height - 1 - pixel_size)
+            swap_width_1 = random.randint(
+                0 + pixel_size, im_width - 1 - pixel_size)
+            swap_height_2 = random.randint(
+                0 + pixel_size, im_height - 1 - pixel_size)
+            swap_width_2 = random.randint(
+                0 + pixel_size, im_width - 1 - pixel_size)
             # image_arr[swap_height_2][swap_width_2], image_arr[swap_height_1][swap_width_1] = image_arr[swap_height_1][swap_width_1], image_arr[swap_height_2][swap_width_2]
             for i in range(swap_height_1 - 10, swap_height_1):
                 for j in range( swap_width_1 - 10, swap_width_1):
