8e7a162c218f5d45354052c1a6c67a84b93b3569
Grace Hseu <25470155+gracehseu@users.noreply.github.com>
2021-11-08 20:38:53 -0600

    changed runtime.txt

--- app/main_module/controllers.py
+++ app/main_module/controllers.py
@@ -8,7 +8,7 @@ main_bp = Blueprint('main_page', __name__)
 @main_bp.route("/", methods=["GET", "POST"])
 def main():
     print('in module_ex_1_bp')
-    image_name = "Judith.jpeg"
+    image_name = "Love of Winter.jpeg"
     location = "images"
 
     return render_template("main_module/module.html", image_name="{}/{}".format(location, image_name), altered_image_name="{}/{}{}".format(location, "altered", image_name))
